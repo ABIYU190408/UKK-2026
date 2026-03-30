e3f2c290a8968be744094f43fa3209c635fdb067
ABIYU190408 <khalilarisda@gmail.com>
2026-03-30 23:31:12 +0700

    Delete UKK-2026 directory

--- UKK-2026/db.php
+++
@@ -1,8 +0,0 @@
-<?php
- $hostname = 'localhost'    ;
- $localhost= 'root'        ;
- $password = ''           ;
- $dbname   = 'db_aspirasi';
-
-$conn = mysqli_connect ( $hostname,$localhost,$password,$dbname) or die ('koneksi gagal');  
-?>
